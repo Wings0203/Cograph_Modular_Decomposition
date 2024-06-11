dbd5f0c277227ef85a00f7ea5f8c552c6efc1498
Wings0203 <57627842+Wings0203@users.noreply.github.com>
2024-06-11 20:10:43 +0300

    Update Cograph_modular_decomposition.py

--- Cograph_modular_decomposition.py
+++ Cograph_modular_decomposition.py
@@ -3,21 +3,17 @@ from sage.all import Graph,graphs
 from Cograph_recognition import is_cograph
 import itertools
 
-def cograph_modular_decomposition(inp):
+def is_cograph(g):
+	P4 = graphs.PathGraph(4)
+	return not g.subgraph_search(P4, induced=True)
+
+def create_cotree(g):
 	"""
  	function that computes the cotree of a given graph
 
- 	two input options:
- 	--- 1. sage graph / python dictionary, with the nodes and ALL their neighbors
-	--- 2. a list containing the names of the nodes and a nested list containing the "eliminated" neighbors of the nodes,
-  	       meaning that a node can have as neigbors only the nodes that precede in the given order.
+ 	input:
+ 	sage graph / python dictionary, with the nodes and ALL their neighbors
 	"""
-	if type(inp)==type([]): 
-		return create_cotree_2(inp)
-	else: 
-		return create_cotree_1(inp)
-
-def create_cotree_1(g):
 	if is_cograph(g)==False:
 		return
 	cotree=Tree('1')
@@ -48,33 +44,4 @@ def create_cotree_1(g):
 				return
 			# initialize tree info for the next iteration
 			cotree.reset_info()
-	return cotree
-
-def create_cotree_2(inp):
-	name=inp[0]
-	neighbors=inp[1]
-	cotree=Tree('1')
-	# the root of the tree is always '1'
-	for i in range(len(name)):
-		if i==1:
-			if name[0] in neighbors[1]:
-				# first and second node are adjacent
-				Tree.add_child(cotree,Tree(name[0]))
-				Tree.add_child(cotree,Tree(name[1]))
-			else:
-				# first and second node are not adjacent
-				tree0=Tree('0',[Tree(name[0]),Tree(name[1])])
-				Tree.add_child(cotree,tree0)
-		elif i!=0:
-			# add incrementally nodes 3,...,end to the cotree
-			flag_mixed=[0]
-			Tree.is_cograph(cotree,name[i],neighbors[i],flag_mixed)
-			if flag_mixed[0]==0:
-				Tree.update_cotree(cotree,name[i],neighbors[i],0,0)
-			elif flag_mixed[0]==2:
-				print ("The input graph is not a co-graph. Execution terminated!")
-				return
-			if i<len(name)-1:
-				# initialize tree info for the next iteration
-				cotree.reset_info()
 	return cotree
